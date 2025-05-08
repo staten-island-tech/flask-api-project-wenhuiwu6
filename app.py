68d5ac0e8f602a1b3d82ce17b236db33b52b266f
wenhuiwu6 <wenhuiwu678@gmail.com>
2025-05-08 09:24:59 -0400

    1

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    response = requests.get("https://api.jikan.moe/v4/top/anime?type=ona")
